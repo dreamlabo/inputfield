98ce145cdf6fcc9d18f901635139a01a11bf0996
dreamlabo <toddlabo@gmail.com>
2023-11-15 22:21:37 -0700

    Working on dark theme.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const fakeFormParagraph =  document.getElementById("fake-form-p");
+
 function openFileBrowser() {
     const hiddenFileBtn = document.getElementById("hidden-file-btn");
     hiddenFileBtn.click();
@@ -10,16 +12,22 @@ hiddenFileBtn.addEventListener("change", () =>  {
         document.getElementById("file-name-text").textContent = hiddenFileBtn.files[0].name
         document.getElementById("btn-browse").blur();
         document.getElementById("btn-trash-can").focus();
-        document.getElementById("fake-form-p").classList.add("active");
+
         document.getElementById("btn-trash-can").classList.add("make-visible");
         document.getElementById("nextBtnCsv").disabled = false;
+
+        fakeFormParagraph.classList.add("active");
+        fakeFormParagraph.setAttribute('aria-hidden','false');
     }
 })
 
 function clearSelectedFile() {
     document.getElementById("form").reset();
     document.getElementById("file-name-text").textContent = "No file selected";
-    document.getElementById("fake-form-p").classList.remove("active");
+
     document.getElementById("btn-trash-can").classList.remove("make-visible");
     document.getElementById("nextBtnCsv").disabled = true;
+
+    fakeFormParagraph.classList.remove("active");
+    fakeFormParagraph.setAttribute('aria-hidden','true');
 }
