d9dd3f26493f6df1eea492b177b5fc78deaa1b7c
dreamlabo <toddlabo@gmail.com>
2023-11-17 13:30:16 -0700

    Reordered the 'switch' button.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 const fakeFormParagraph =  document.getElementById("fake-form-p");
+const hiddenFileBtn = document.getElementById("hidden-file-btn");
+const toggleButton = document.getElementById("toggle-button");
+let darkMode = true;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const myButton = document.getElementById('nextBtnCsv');
+    myButton.disabled = true; 
+});
 
 function openFileBrowser() {
     const hiddenFileBtn = document.getElementById("hidden-file-btn");
     hiddenFileBtn.click();
 }
-
-const hiddenFileBtn = document.getElementById("hidden-file-btn");
 
 hiddenFileBtn.addEventListener("change", () =>  {
     if(hiddenFileBtn.files.length > 0 ) {
@@ -23,11 +29,27 @@ hiddenFileBtn.addEventListener("change", () =>  {
 
 function clearSelectedFile() {
     document.getElementById("form").reset();
-    document.getElementById("file-name-text").textContent = "No file selected";
+    document.getElementById("file-name-text").textContent = "No CSV file selected";
 
     document.getElementById("btn-trash-can").classList.remove("make-visible");
     document.getElementById("nextBtnCsv").disabled = true;
 
     fakeFormParagraph.classList.remove("active");
     fakeFormParagraph.setAttribute('aria-hidden','true');
+}
+
+function toggleColorTheme() {
+    darkMode ? setLightMode() : setDarkMode();
+}
+
+function setLightMode() {
+    document.documentElement.setAttribute("color-mode", 'light');
+    toggleButton.innerHTML = "Switch to dark mode"
+    darkMode = false;
+}
+
+function setDarkMode() {
+    document.documentElement.setAttribute("color-mode", "dark");
+    toggleButton.innerHTML = "Switch to light mode"
+    darkMode = true;
 }
